d1b36b44782781adcc7c7768960779df297a9cf9
Lojain Abdalrazaq <94284765+Lojain-Abdalrazaq@users.noreply.github.com>
2026-06-16 18:36:15 +0300

    Add: isomorphic-strings - submission-0

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_map = {}
+        t_map = {}
+
+        for i in range(len(s)):
+            s_char = s[i]
+            t_char = t[i]
+
+            if s_char not in s_map:
+                s_map[s_char] = t_char
+            elif s_map[s_char] != t_char:
+                return False
+            
+            if t_char not in t_map:
+                t_map[t_char] = s_char
+            elif t_map[t_char] != s_char:
+                return False
+        
+        return True
+        
