df63eb1df172244885162438b737d693fae7cf53
Lojain Abdalrazaq <94284765+Lojain-Abdalrazaq@users.noreply.github.com>
2026-09-02 13:22:38 +0300

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # Approach #1: Using hashmap
+        # time and space complexity: O(n)
+        # freq_map = {}
+        # for num in nums:
+        #     if num in freq_map:
+        #         freq_map[num] +=1 
+        #         # checking the freq
+        #     else:
+        #         freq_map[num] = 1
+
+        #     if freq_map[num] > len(nums) // 2:
+        #             return num
+        
+        # ==================================
+        # Approach #2: Using Boyer Moore Majority Element
+        # Time -> O(n)
+        # Space -> O(1) # no space needed
+        condidate = None
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                condidate = num
+            if num == condidate:
+                count += 1
+            else:
+                count -= 1
+        
+        return condidate
+            
